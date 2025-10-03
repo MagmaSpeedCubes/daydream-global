0aa6cd11a8f3ed0add72224315c70116d7327649
MagmaSpeedCubes <110320002+MagmaSpeedCubes@users.noreply.github.com>
2025-10-03 00:18:34 -0700

    some experiments in drag and drop

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    [SerializeField] private Canvas canvas;
+    
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1.0f;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+    }
+}
